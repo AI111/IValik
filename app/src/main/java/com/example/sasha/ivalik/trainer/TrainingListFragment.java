afc3b9ad1fe202d88bc7fc630a1aef728ca97b9f
AN225 <sashaandreev7@gmail.com>
2015-02-19 10:01:42 +0200

    add training fragment

---
+++ app/src/main/java/com/example/sasha/ivalik/trainer/TrainingListFragment.java
@@ -0,0 +1,165 @@
+package com.example.sasha.ivalik.trainer;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.example.sasha.ivalik.MainActivity;
+import com.example.sasha.ivalik.R;
+import com.example.sasha.ivalik.models.CustomExercise;
+import com.example.sasha.ivalik.models.Exercise;
+import com.example.sasha.ivalik.registration.OnItemClicklistener;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sasha on 2/10/15.
+ */
+public class TrainingListFragment extends Fragment implements View.OnClickListener {
+    public ArrayList<CustomExercise> exercises;//= new ArrayList<>();
+    ImageButton addBtn;
+
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private MyAdapter mAdapter;
+    //  ListView listView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.training_list_fragment, container, false);
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.my_recycler_view);
+//        mRecyclerView.setItemViewCacheSize(6);
+        final RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        //mRecyclerView.setLayoutManager(mLayoutManager);
+        RecyclerView.ItemAnimator itemAnimator = new DefaultItemAnimator();
+        mRecyclerView.setItemAnimator(itemAnimator);
+//        // specify an adapter (see also next example)
+        // listView = (ListView) rootView.findViewById(R.id.list_view);
+        exercises = new ArrayList<>();
+        exercises.add(new CustomExercise(
+                new Exercise(R.string.pull_ups_on_the_bar, (byte) 0, R.string.pull_ups_on_the_bar_desc, "o_b189b90655199e57-0.jpg", "o_b189b90655199e57-1.jpg")
+                , (byte) 10, (byte) 10, (byte) 80, false));
+        exercises.add(new CustomExercise(
+                new Exercise(R.string.dips, (byte) 0, R.string.dips_description, "o_4add3c18dd626cdc-0.jpg", "o_4add3c18dd626cdc-1.jpg")
+                , (byte) 10, (byte) 10, (byte) 80, false));
+        //  listView
+        mAdapter = new MyAdapter(exercises);
+        // mAdapter.setOnItemClickListener(this);
+//
+        mAdapter.setOnItemClickListener(new OnItemClicklistener() {
+            @Override
+            public void onClickItem(View rootView, View view, int position) {
+                Log.v(MainActivity.LOG_TAG, "OnItemClick  " + position);
+                getFragmentManager().beginTransaction().replace(R.id.container, new ExerciseFragment(exercises.get(position))).addToBackStack(null).commit();
+            }
+        });
+        mRecyclerView.setAdapter(mAdapter);
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(MainActivity.LOG_TAG, "onClick " + exercises.size());
+
+    }
+
+
+    private class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+        ArrayList<CustomExercise> exercises;
+        OnItemClicklistener onItemClicklistener;
+
+        MyAdapter(ArrayList<CustomExercise> exercises) {
+            this.exercises = exercises;
+        }
+
+        public void setOnItemClickListener(OnItemClicklistener onItemClicklistener) {
+            this.onItemClicklistener = onItemClicklistener;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.training_list_item, viewGroup, false);
+            return new MyViewHolder(v, onItemClicklistener);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder viewHolder, int i) {
+            viewHolder.number.setText((i + 1) + "");
+            viewHolder.title.setText(exercises.get(i).getExercise().getNameId());
+            viewHolder.approach.setText(exercises.get(i).getApproach() + "");
+            viewHolder.repeat.setText(exercises.get(i).getRepeat() + "");
+            viewHolder.weight.setText((exercises.get(i).getPercentPM() * exercises.get(i).getExercise().getpM()) + "");
+
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return exercises.size();
+        }
+
+
+    }
+
+    //
+    private class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+
+        OnItemClicklistener onItemClicklistener;
+        View itemView;
+        TextView title, approach, repeat, weight, number;
+        private TextView time;
+        private ToggleButton toggleButton;
+        private LinearLayout layout;
+
+        public MyViewHolder(View itemView, OnItemClicklistener onItemClicklistener) {
+            super(itemView);
+            this.itemView = itemView;
+            this.onItemClicklistener = onItemClicklistener;
+            time = (TextView) itemView.findViewById(R.id.textView4);
+            toggleButton = (ToggleButton) itemView.findViewById(R.id.UP_DOWN);
+            toggleButton.setOnCheckedChangeListener(this);
+            title = (TextView) itemView.findViewById(R.id.textView4);
+            number = (TextView) itemView.findViewById(R.id.textView5);
+            layout = (LinearLayout) itemView.findViewById(R.id.Calendar_feeld);
+            title.setOnClickListener(this);
+            approach = (TextView) itemView.findViewById(R.id.textView7);
+            repeat = (TextView) itemView.findViewById(R.id.textView9);
+            weight = (TextView) itemView.findViewById(R.id.textView11);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (onItemClicklistener != null)
+                onItemClicklistener.onClickItem(itemView, view, getPosition());
+
+        }
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            layout.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+        }
+    }
+}
