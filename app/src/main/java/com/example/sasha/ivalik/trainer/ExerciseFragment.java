cd495f34ac7e57dfc7006f3baa878414b995a9e8
AN225 <sashaandreev7@gmail.com>
2015-02-19 02:43:02 +0200

    add training fragment

--- app/src/main/java/com/example/sasha/ivalik/trainer/ExerciseFragment.java
+++ app/src/main/java/com/example/sasha/ivalik/trainer/ExerciseFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -16,6 +17,7 @@ import android.widget.ToggleButton;
 import com.example.sasha.ivalik.MainActivity;
 import com.example.sasha.ivalik.R;
 import com.example.sasha.ivalik.models.CustomExercise;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by sasha on 2/18/15.
@@ -25,12 +27,12 @@ public class ExerciseFragment extends Fragment implements View.OnClickListener, 
     ToggleButton toggleButton;
     private ProgressBar progressBar;
     private CustomExercise exercise;
-    ;
+    private ImageView img1, img2;
     private TextView description, title, approach, repeat, weight;
 
-    //    public ExerciseFragment(CustomExercise exercise){
-//        this.exercise=exercise;
-//    }
+    public ExerciseFragment(CustomExercise exercise) {
+        this.exercise = exercise;
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,9 +43,15 @@ public class ExerciseFragment extends Fragment implements View.OnClickListener, 
         View rootView = inflater.inflate(R.layout.exercise_detail_fragment, container, false);
         progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
         description = (TextView) rootView.findViewById(R.id.textView15);
+        description.setText(exercise.getExercise().getDescriptionId());
         title = (TextView) rootView.findViewById(R.id.textView12);
-        approach = (TextView) rootView.findViewById(R.id.textView17);
+        title.setText(exercise.getExercise().getNameId());
+        img1 = (ImageView) rootView.findViewById(R.id.imageView);
+        Picasso.with(getActivity()).load("file:///android_asset/" + exercise.getExercise().getUrl1()).into(img1);
 
+        img2 = (ImageView) rootView.findViewById(R.id.imageView2);
+        Picasso.with(getActivity()).load("file:///android_asset/" + exercise.getExercise().getUrl2()).into(img2);
+        approach = (TextView) rootView.findViewById(R.id.textView17);
         toggleButton = (ToggleButton) rootView.findViewById(R.id.UP_DOWN);
         toggleButton.setOnCheckedChangeListener(this);
         progressBar.setMax(5000);
